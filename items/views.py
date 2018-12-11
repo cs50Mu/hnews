f5bbcb0c85aa6fd53d9a342171758a4d569dafdf
linuxfish <linuxfish.exe@gmail.com>
2018-12-11 14:54:23 +0800

    add list view

--- items/views.py
+++ items/views.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views.generic.list import ListView
+
+from items.models import Item
 
 # Create your views here.
+
+class ItemListView(ListView):
+
+    template_name = 'items/list.html'
+    model = Item
+    context_object_name = 'items'
